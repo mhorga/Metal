fe51212d7b96c6f91a34a55c7e0e1c60e600d60a
Marius Horga <mhorga@gmail.com>
2016-10-01 12:55:56 +0300

    final commit

--- ch08/chapter08.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-
-import Cocoa
-import XCPlayground
-
-let device = MTLCreateSystemDefaultDevice()!
-let frame = NSRect(x: 0, y: 0, width: 300, height: 300)
-let view = MetalView(frame: frame, device: device)
-XCPlaygroundPage.currentPage.liveView = view
--- ch12/chapter12.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-
-import Cocoa
-import XCPlayground
-
-let device = MTLCreateSystemDefaultDevice()!
-let frame = NSRect(x:0, y:0, width:300, height:300)
-let metalView = MetalView(frame: frame, device: device)
-XCPlaygroundPage.currentPage.liveView = metalView
